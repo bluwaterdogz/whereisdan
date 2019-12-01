b0b3cc67c026398bbf6d5fb29c10b7f461330af7
bluwaterdogz <ps@macbooks-MacBook-Pro.local>
2019-12-01 15:14:09 -0800

    Updates

---
+++ precache-manifest.7ea1948c726d78867b4d56f7412a3eaf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "940249408b5425633483cc02e3999834",
+    "url": "/whereisdan/index.html"
+  },
+  {
+    "revision": "0fc2152435917f87718a",
+    "url": "/whereisdan/static/css/main.eb372e32.chunk.css"
+  },
+  {
+    "revision": "321476cd8184d1b82f8c",
+    "url": "/whereisdan/static/js/2.1d47ea7a.chunk.js"
+  },
+  {
+    "revision": "0fc2152435917f87718a",
+    "url": "/whereisdan/static/js/main.2e40fc2e.chunk.js"
+  },
+  {
+    "revision": "2344c7ca38fadcbfbec9",
+    "url": "/whereisdan/static/js/runtime-main.fbb29a56.js"
+  },
+  {
+    "revision": "fab9d32a3e48d3d59a6d2f169e2bb861",
+    "url": "/whereisdan/static/media/dan.fab9d32a.png"
+  }
+]);
